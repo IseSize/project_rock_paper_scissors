eeb3eac946a55e212f44a6f6c9d0f4479880515c
IseSize <127502987+IseSize@users.noreply.github.com>
2024-04-28 23:26:23 +0200

    feat(scripts): define computer choice function
    Define getComputerChoice() that randomly returns paper, rock or sciss.

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,8 @@
 // PRINT WELCOME MESSAGE
+console.log("Welcome to Rock - Paper - Scissors game !");
 
 // WHILE USER WANT TO PLAY
+
 //    ASK USER NUMBER OF ROUNDS TO PLAY
 //    SET VAR rounds FOR ROUNDS TO PLAY WITH USER INPUT
 //    SET VAR score FOR SCORE
@@ -14,3 +16,16 @@
 // END WHILE
 // PRINT QUIT MESSAGE
 
+function getComputerChoice() {
+    // randomly return rock, paper or scissors
+    let computerChoice;
+    let randomNumber = Math.random();
+    if (randomNumber < 1/3) {  // Divide [0;1] range in 3 parts and look where randomNumber is 
+        computerChoice = "rock";
+    } else if (randomNumber < 2/3) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+    return computerChoice;
+}
