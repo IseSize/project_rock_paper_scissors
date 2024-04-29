f32d90a2055cb772adb08fb68ecc49e60d100734
IseSize <127502987+IseSize@users.noreply.github.com>
2024-04-29 12:07:24 +0200

    feat & fix(scripts) : add playRound function & minor changes in pseudocode/score system
    Define playRound function, add 2 subfonction to (1)format
    output and (2)print score.
    Fix pseudocode, change score system by 2 different var in global
    scope.

--- scripts/game.js
+++ scripts/game.js
@@ -1,18 +1,30 @@
 // PRINT WELCOME MESSAGE
+console.log("_".repeat(50));
 console.log("Welcome to Rock - Paper - Scissors game !");
+console.log("_".repeat(50));
 
+let userGameStart = parseInt(prompt("Enter 1 to play."));
+let gameState = userGameStart == 1 ? true : false;
 // WHILE USER WANT TO PLAY
-
+while (gameState) {
 //    ASK USER NUMBER OF ROUNDS TO PLAY
 //    SET VAR rounds FOR ROUNDS TO PLAY WITH USER INPUT
-//    SET VAR score FOR SCORE
-//    WHILE NOT rounds ROUNDS ARE PLAYED
-//      PLAY ROUND BY CALLING playRound() FUNCTION
-//    END WHILE
+//    SET VARS FOR SCORE
+    var userScore = 0;
+    var computerScore = 0;
+    let rounds = parseInt(prompt("How many rounds do you want to play ? "));
+//    WHILE NOT {rounds} ROUNDS ARE PLAYED
+    while(rounds > 0) {
+        //PLAY ROUND BY CALLING playRound() FUNCTION
+        playRound();
+        rounds--;
+    }
+//  END WHILE
 // PRINT score SCORE
 // ASK USER TO REPLAY OR QUIT
 // IF QUIT
 //    BREAK LOOP
+}
 // END WHILE
 // PRINT QUIT MESSAGE
 
@@ -42,4 +54,50 @@ function getUserChoice() {
             userChoice = "notValid";
         }
     }
+}
+
+function playRound() {
+    /* Play a round :
+    Get user choice, Get computer choice
+    Compare user & computer choice and update score
+    Print result & score */
+
+    let computerChoice = getComputerChoice();
+    let userChoice = getUserChoice();
+
+    //Format text for output
+    let userChoiceOut = get_formated_choice_text_for_output(userChoice);
+    let computerChoiceOut = get_formated_choice_text_for_output(computerChoice);
+
+    console.log("User choice : " + userChoiceOut + " | Computer choice : " + computerChoiceOut);
+    console.log(userChoiceOut + " vs " + computerChoiceOut + " !");
+
+    //Compare situation, determin winner, set score and print formatted text
+    if (userChoice === computerChoice) {
+        console.log("It's a draw!");
+    } else {
+        if((userChoice === "paper" && computerChoice === "rock") || 
+           (userChoice === "scissors" && computerChoice === "paper") ||
+           (userChoice === "rock" && computerChoice === "scissors")) {
+            console.log("User win ! " + userChoiceOut + " > " + computerChoiceOut);
+            userScore++;
+        } else {
+            console.log("Computer win ! " + computerChoiceOut + " > " + userChoiceOut);
+            computerScore++;
+        }
+    }
+    printScore(userScore, computerScore);
+    console.log("_".repeat(30));
+    console.log(" ");
+    
+}
+
+
+function get_formated_choice_text_for_output(choice) {
+    return choice.slice(0,1).toUpperCase() + choice.slice(1);
+}
+
+function printScore(userScore, computerScore) {
+    console.log(" ".repeat(10) + "| Score |" + " ".repeat(10));
+    console.log(" ".repeat(5) + "User : " + userScore + " | Computer : " + computerScore);
 }
