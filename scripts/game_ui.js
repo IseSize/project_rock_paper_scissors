100581d16f2239e51620884cceb9e16738665545
IseSize <127502987+IseSize@users.noreply.github.com>
2024-05-07 09:17:33 +0200

    feat!: add result screen, add game state arch.
    BREAKING CHANGE: Game is now based on states and
    a function game(gameState) that manages them,
    called by events.

--- scripts/game_ui.js
+++ scripts/game_ui.js
@@ -14,78 +14,108 @@ const numberRoundText = document.querySelector("#numberRound");
 const userScoreText = document.querySelector("#userScore");
 const computerScoreText = document.querySelector("#computerScore");
 const characterSelection = document.querySelectorAll(".character");
-const versus = document.querySelector("#versus");
+const versusMenu = document.querySelector("#versus");
+const vsBar = document.querySelector("#vsBar");
 const userCharacter = document.querySelector("#userCharacter");
 const computerCharacter = document.querySelector("#computerCharacter");
+
+const winUserCharacter = document.querySelector("#winUserCharacter");
+const winComputerCharacter = document.querySelector("#winComputerCharacter");
+const loseUserCharacter = document.querySelector("#loseUserCharacter");
+const loseComputerCharacter = document.querySelector("#loseComputerCharacter");
+const drawUserCharacter = document.querySelector("#drawUserCharacter");
+const drawComputerCharacter = document.querySelector("#drawComputerCharacter");
+
+const winMenu = document.querySelector("#win");
+const loseMenu = document.querySelector("#lose");
+const drawMenu = document.querySelector("#draw");
 
 var userScore = 0;
 var computerScore = 0;
 var rounds = 0;
 var actualRound = 1;
-var gameState = false;
+var roundState = "";
 var computerChoice = "";
+var userChoice ="";
+
 playButton.addEventListener("mouseover", () => {
     playButton.style.top = "-10px";
     playButton.style.cursor = "pointer";
     playText.style.color = "#3A2C22";
-    console.log('hover');
 });
 playButton.addEventListener("mouseout", () => {
     playButton.style.top = "0px";
     playText.style.color = "#684F3A";
-    console.log('hover');
 });
 
-playButton.addEventListener("click", () => {
-    startGame();
+playButton.addEventListener("click", (e) => {
+    game("init");
+    e.stopPropagation();
 });
 
 characterSelection.forEach((elem) => {
-    elem.addEventListener("click", () => {
+    elem.addEventListener("click", (e) => {
         userChoice = elem.id;
         computerChoice = getComputerChoice();
-        playRound(userChoice, computerChoice);
+        game("versusScreen",selectMenu);
+        e.stopPropagation();
     });
 });
 
+function game(gameState, previousMenu) {
+    switch(gameState) {
+        case "init":
+            startGame();
+            game("selectChar", mainMenu);
+            break;
+        case "selectChar":
+            hideMenu(previousMenu);
+            showSelectMenu(actualRound, rounds, userScore, computerScore);
+            break;
+        case "versusScreen":
+            hideMenu(previousMenu);
+            showMenu(versusMenu);
+            playRound(userChoice, computerChoice);
+            actualRound++;
+            waitOrClic("resultScreen", versusMenu, 2000);
+            break;
+        case "resultScreen":
+            hideMenu(previousMenu);
+            let menuToHide;
+            switch(roundState) {
+                case "win":
+                    showMenu(winMenu); 
+                    menuToHide = winMenu;
+                    break;
+                case "lose":
+                    showMenu(loseMenu); 
+                    menuToHide = loseMenu;
+                    break;
+                case "draw":
+                    showMenu(drawMenu); 
+                    menuToHide = drawMenu;
+                    break;
+            }
 
+            if(actualRound <= rounds) {
+                waitOrClic("selectChar", menuToHide, 2000);
+            } else {
+                waitOrClic("finalScreen", menuToHide, 2000);
+            }
+            break;
+        case "finalScreen":
+                console.log("final screen");
+                console.log("_".repeat(30));
+                console.log(" ");
+            break;
+    }
+}
 function startGame() {
     userScore = 0;
     computerScore = 0;
     rounds = parseInt(roundSelection.value);
-    actualRound = 1;
-    hideMainMenu();
-    showSelectMenu(actualRound, rounds, userScore, computerScore);
-    //     console.log("End !");
-    // // PRINT SCORE
-    //     console.log("Final score :");
-    //     printScore(userScore, computerScore);
-    //     if (userScore == computerScore) {
-    //         console.log("Oh ! It's a Draw. ");
-    //     } else if (userScore > computerScore) {
-    //         console.log("You win ! Congrats !");
-    //     } else {
-    //         console.log("You lose... Maybe next time ?");
-    //     }
-    //     console.log(" ".repeat(20));
-    // // REPLAY OR QUIT
-    //     let replay = prompt("Do you want to play again ? YES / NO").trim().toLowerCase();
-    //     if(replay === "no") {
-    //         break;
-    //     }
-    
-    console.log(" ".repeat(20));
-    console.log("Thanks for playing ! See you. ")
-    
-    ////////////////////////////////////////////////
+    actualRound = 1;   
 }
-
-// let userGameStart = parseInt(prompt("Enter 1 to play."));
-// let gameState = userGameStart == 1 ? true : false;
-
-///////////GAME LOOP/////////////////////////////
-
-
 
 function getComputerChoice() {
     let computerChoice;
@@ -101,32 +131,33 @@ function getComputerChoice() {
     
 }
 
-
-
 function playRound(userChoice, computerChoice) {
-    /* Play a round :
-    Get user choice, Get computer choice
-    Compare user & computer choice and update score
-    Print result & score */
-
-    //Compare situation, determine winner, set score and print formatted text
-    hideSelectMenu();
-    showVersusMenu();
+    
     computerCharacter.setAttribute("src", "assets/images/character/"+computerChoice+".svg");
     userCharacter.setAttribute("src", "assets/images/character/"+userChoice+".svg");
+
     if (userChoice === computerChoice) {
-        console.log("It's a draw!");
+        drawUserCharacter.setAttribute("src", "assets/images/character/"+userChoice+".svg");
+        drawComputerCharacter.setAttribute("src", "assets/images/character/"+computerChoice+".svg");
+        roundState = "draw";
+        
     } else {
         if((userChoice === "paper" && computerChoice === "rock") || 
            (userChoice === "scissors" && computerChoice === "paper") ||
            (userChoice === "rock" && computerChoice === "scissors")) {
-            console.log("You win !");
+            winUserCharacter.setAttribute("src", "assets/images/character/"+userChoice+".svg");
+            winComputerCharacter.setAttribute("src", "assets/images/character/"+computerChoice+"Lose.svg");
             userScore++;
+            roundState = "win";
         } else {
-            console.log("You loose ... ");
+            loseUserCharacter.setAttribute("src", "assets/images/character/"+userChoice+"Lose.svg");
+            loseComputerCharacter.setAttribute("src", "assets/images/character/"+computerChoice+".svg");
             computerScore++;
+            roundState = "lose";
         }
     }
+    
+
     printScore(userScore, computerScore);
     console.log("_".repeat(30));
     console.log(" ");
@@ -134,15 +165,21 @@ function playRound(userChoice, computerChoice) {
 }
 
 
-function get_formated_choice_text_for_output(choice) {
-    //return choice capitalize first letter
-    return choice.slice(0,1).toUpperCase() + choice.slice(1);
-}
-
 
 function printScore(userScore, computerScore) {
     console.log(" ".repeat(10) + "| Score |" + " ".repeat(10));
     console.log(" ".repeat(5) + "You : " + userScore + " | Computer : " + computerScore);
+}
+
+function hideMenu(menu) {
+    if(menu !== undefined) {
+        menu.style.display = "none";
+    }
+}
+function showMenu(menu) {
+    if(menu !== undefined) {
+        menu.style.display = "block";
+    }
 }
 
 function showSelectMenu(actualRound, round, userScore, computerScore) {
@@ -152,12 +189,20 @@ function showSelectMenu(actualRound, round, userScore, computerScore) {
     userScoreText.textContent = userScore;
     computerScoreText.textContent = computerScore;
 }
-function hideMainMenu() {
-    mainMenu.style.display = "none";
+
+
+function waitOrClic(nextGameState, actualMenu, timeInMs) {
+    let timeOutId;
+    function clickToPass() {
+        clearTimeout(timeOutId);
+        document.removeEventListener('click', clickToPass);
+        game(nextGameState, actualMenu);
+    }
+    document.addEventListener("click", clickToPass);
+
+    timeOutId = setTimeout(() => {
+            game(nextGameState, actualMenu);
+            document.removeEventListener('click', clickToPass);
+        }, timeInMs);
 }
-function hideSelectMenu() {
-    selectMenu.style.display = "none"
-}
-function showVersusMenu() {
-    versus.style.display="block";
-}
+
